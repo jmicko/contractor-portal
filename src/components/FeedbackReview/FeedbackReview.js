31dcee12cc4aab5f9a4cba1ebe9c6a2adb3a455c
John Micko <jrmicko5012@gmail.com>
2021-01-21 05:27:50 -0600

    split feedback component into admin/client compon

--- src/components/FeedbackReview/FeedbackReview.js
+++ src/components/FeedbackReview/FeedbackReview.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
-import ClientFeedback from '../ClientFeedback/ClientFeedback';
-import FeedbackReview from '../FeedbackReview/FeedbackReview';
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
 // the component name TemplateClass with the name for the new
 // component.
-class Feedback extends Component {
+class FeedbackReview extends Component {
   state = {
-    heading: 'Feedback',
+    heading: 'Review Feedback',
   };
+
+  // todo - GET feedback items from DB
+  componentDidMount() {
+    const action = {type: 'GET_FEEDBACK'};
+    this.props.dispatch(action);
+}
+  // map over returned data 
+  // create component for each item
 
   render() {
     return (
       <div>
         <h2>{this.state.heading}</h2>
-        {/* todo - conditionally render components based on if user is admin or not */}
-        <ClientFeedback />
-        <FeedbackReview />
       </div>
     );
   }
 }
 
-export default connect(mapStoreToProps)(Feedback);
+export default connect(mapStoreToProps)(FeedbackReview);
