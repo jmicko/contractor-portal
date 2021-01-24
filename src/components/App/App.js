87611e9ca983959b0c6e454963d172c1ed147752
John Micko <jrmicko5012@gmail.com>
2021-01-24 13:28:07 -0600

    remove unused components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,9 +13,6 @@ import Footer from '../Footer/Footer';
 
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
-import AboutPage from '../AboutPage/AboutPage';
-import UserPage from '../UserPage/UserPage';
-import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
@@ -85,17 +82,6 @@ class App extends Component {
                 exact
                 path="/history"
                 component={History}
-                />
-
-              {/* For protected routes, the view could show one of several things on the same route.
-                Visiting localhost:3000/user will show the UserPage if the user is logged in.
-                If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
-              Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-              <ProtectedRoute
-                // logged in shows UserPage else shows LoginPage
-                exact
-                path="/user"
-                component={UserPage}
                 />
               {/* When a value is supplied for the authRedirect prop the user will
                 be redirected to the path supplied when logged in, otherwise they will
