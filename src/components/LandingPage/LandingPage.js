c8071e686289c599bab8692f03c3cd4ed28a0e40
John Micko <jrmicko5012@gmail.com>
2021-01-24 07:53:45 -0600

    conditionally render login on home page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -47,14 +47,21 @@ class LandingPage extends Component {
             </p>
           </div>
           <div className="grid-col grid-col_4">
-            <RegisterForm />
-
-            <center>
-              <h4>Already a Member?</h4>
-              <button className="btn btn_sizeSm" onClick={this.onLogin}>
-                Login
+            {this.props.store.user.id != null
+              ? ''
+              :
+              <div>
+                <RegisterForm />
+                <center>
+                  <h4>Already a Member?</h4>
+                  <button className="btn btn_sizeSm" onClick={this.onLogin}>
+                    Login
               </button>
-            </center>
+                </center>
+              </div>
+            }
+
+
           </div>
         </div>
       </div>
