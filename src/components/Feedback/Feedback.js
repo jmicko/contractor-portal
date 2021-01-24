cbeda6096577466f3ecbb5ac8045276af4fdf0ef
John Micko <jrmicko5012@gmail.com>
2021-01-24 13:31:15 -0600

    remove old notes

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -4,10 +4,6 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 import ClientFeedback from '../ClientFeedback/ClientFeedback';
 import FeedbackReview from '../FeedbackReview/FeedbackReview';
 
-// Basic class component structure for React with default state
-// value setup. When making a new component be sure to replace
-// the component name TemplateClass with the name for the new
-// component.
 class Feedback extends Component {
   state = {
     heading: 'Feedback',
@@ -17,8 +13,6 @@ class Feedback extends Component {
     return (
       <div>
         <h2>{this.state.heading}</h2>
-        {/* todo - conditionally render components based on if user is admin or not */}
-        {/* User is admin: {JSON.stringify(this.props.store.user.is_admin)} */}
         {this.props.store.user.is_admin
         ? <FeedbackReview />
         : <ClientFeedback />
