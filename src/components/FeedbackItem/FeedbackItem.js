88605c056f37a05c5e6155c4c12aed8dec6d64c1
John Micko <jrmicko5012@gmail.com>
2021-01-26 22:52:15 -0600

    notes

--- src/components/FeedbackItem/FeedbackItem.js
+++ src/components/FeedbackItem/FeedbackItem.js
@@ -12,6 +12,7 @@ class TemplateClass extends Component {
     heading: 'Feedback Item Component',
   };
 
+  // Call delete feedback saga, pass feedback id into it as payload
   deleteFeedback = () => {
     console.log('delete feedback button clicked! feedbackItem id is:', this.props.feedbackItem.id);
     const action = {
@@ -20,7 +21,8 @@ class TemplateClass extends Component {
     };
     this.props.dispatch(action);
   }
-
+  
+  // Call feature feedback saga, pass feedback id into it as payload
   featureFeedback = () => {
     console.log('feature feedback button clicked! feedbackItem id is:', this.props.feedbackItem.id);
     const action = {
@@ -33,29 +35,37 @@ class TemplateClass extends Component {
   render() {
     return (
       <div className="feedback-container">
-        {/* {JSON.stringify(this.props.feedbackItem.is_public)} */}
+        {/* show uploaded picture if available */}
+        {/* todo - account for multiple pictures */}
         <div className="image-panel">
           <img src={this.props.feedbackItem.image_url} alt={'project'}></img>
         </div>
         <div className="text-panel">
+          {/* display relevant info from feedback */}
           <p>Feedback ID: {this.props.feedbackItem.id}</p>
           <p><strong>Name:</strong> {this.props.feedbackItem.first_name + " " + this.props.feedbackItem.last_name}</p>
           <p><strong>Rating: </strong>{this.props.feedbackItem.rating}/5</p>
           <p><strong>Comments: </strong>{this.props.feedbackItem.comments}</p>
+          {/* todo - display permission as a flag maybe? do not show feature button if permission is not granted */}
           {/* <p><strong>Allowed to share publicly: </strong>{String(this.props.feedbackItem.ok_to_share)}</p> */}
           {/* <p><strong>Public? </strong>{String(this.props.feedbackItem.is_public)}</p> */}
         </div>
+        {/* check parent component, only show buttons if on feedback review page */}
         {this.props.parent === 'FeedbackReview'
           ? 
           <div className="button-panel">
             <button className="btn btn-feedback" onClick={this.featureFeedback}>
+              {/* toggle button text if feedback is shown on home page or not */}
               {this.props.feedbackItem.is_public
               ? 'Remove from Home'
               : 'Feature on Home'
               }
               </button>
+              {/* delete button triggers delete feedback saga */}
             <button className="btn btn-feedback" onClick={this.deleteFeedback}>Delete</button>
           </div>
+          // show no buttons if not on the review page
+              // todo - possibly allow users to delete feedback? only show delete button for them
           :
           <></>
         }
